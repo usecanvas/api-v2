73747e50e03336aa3046cbcb0811beb82cc3bb8e
Jonathan Clem <jonathan@jclem.net>
2016-09-23 12:44:50 -0400

    Remove cache_static_manifest

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,6 @@ use Mix.Config
 config :canvas_api, CanvasAPI.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("HOST"), port: 80],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
   # force_ssl: [hsts: true]
 
