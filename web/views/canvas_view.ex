a8a854db954bd5902e06ee30cc6fa60c435dc352
Jonathan Clem <jonathan@jclem.net>
2016-10-04 12:31:58 -0400

    Revert "Do not return blocks in JSON API"
    This reverts commit de8b501394663081364934ca95d0e9e2853ecc31.

--- web/views/canvas_view.ex
+++ web/views/canvas_view.ex
@@ -1,8 +1,6 @@
 defmodule CanvasAPI.CanvasView do
   use CanvasAPI.Web, :view
 
-  alias CanvasAPI.Canvas
-
   def render("index.json", %{canvases: canvases}) do
     %{
       data: render_many(canvases, CanvasAPI.CanvasView, "canvas.json"),
@@ -42,11 +40,10 @@ defmodule CanvasAPI.CanvasView do
     %{
       id: canvas.id,
       attributes: %{
+        blocks: canvas.blocks,
         is_template: canvas.is_template,
         native_version: canvas.native_version,
         slack_channel_ids: canvas.slack_channel_ids,
-        summary: Canvas.summary(canvas),
-        title: Canvas.title(canvas),
         type: canvas.type,
         version: canvas.version,
         inserted_at: canvas.inserted_at,
