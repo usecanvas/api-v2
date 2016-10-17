850d47edd4eacd7819d088c71a2c29d0a2baa3b1
Jonathan Clem <jotclem@gmail.com>
2016-10-17 13:52:46 -0400

    Canvas unfurl respects filter query param

--- lib/canvas_api/unfurl/canvas.ex
+++ lib/canvas_api/unfurl/canvas.ex
@@ -8,8 +8,15 @@ defmodule CanvasAPI.Unfurl.Canvas do
     "https://s3.amazonaws.com/canvas-assets/provider-icons/canvas.png")
   @provider_url "https://usecanvas.com"
 
-  @canvas_regex Regex.compile!(
-    "\\A#{System.get_env("WEB_URL")}/[^/]+/(?<id>[^/]{22})\\z")
+  @canvas_regex Regex.compile! """
+  \\A#{System.get_env("WEB_URL")}/
+  [^/]+/
+  (?<id>[^/]{22})
+  (?:#[^\\?]+)?
+  (?:\\?.+)?
+  \\z
+  """
+  |> String.replace("\n", "")
 
   alias CanvasAPI.{Block, Canvas, Repo, Unfurl}
   alias Unfurl.Field
@@ -17,25 +24,26 @@ defmodule CanvasAPI.Unfurl.Canvas do
   def unfurl(url, _opts) do
     with id when is_binary(id) <- extract_canvas_id(url),
          canvas = %Canvas{} <- Repo.get(Canvas, id) |> Repo.preload([:team]) do
+
+      blocks = filter_blocks(canvas.blocks, get_query(url)["filter"])
+
       %Unfurl{
         id: url,
         title: canvas_title(canvas),
-        text: canvas_summary(canvas),
+        text: canvas_summary(blocks),
         provider_name: @provider_name,
         provider_icon_url: @provider_icon_url,
         provider_url: @provider_url,
         url: "#{System.get_env("WEB_URL")}/#{canvas.team.domain}/#{id}",
-        fields: progress_fields(canvas)
+        fields: progress_fields(blocks)
       }
     end
   end
 
   def canvas_regex, do: @canvas_regex
 
-  defp canvas_summary(canvas) do
-    first_content_block =
-      canvas.blocks
-      |> Enum.at(1)
+  defp canvas_summary(blocks) do
+    first_content_block = Enum.at(blocks, 0)
 
     case first_content_block do
       %Block{blocks: [block | _]} ->
@@ -53,8 +61,8 @@ defmodule CanvasAPI.Unfurl.Canvas do
     |> Map.get(:content)
   end
 
-  defp progress_fields(canvas) do
-    {complete, total} = get_progress(canvas.blocks)
+  defp progress_fields(blocks) do
+    {complete, total} = blocks |> get_progress
 
     [%Field{title: "Tasks Complete", value: complete, short: true},
      %Field{title: "Tasks Total", value: total, short: true}]
@@ -76,9 +84,35 @@ defmodule CanvasAPI.Unfurl.Canvas do
     end)
   end
 
+  defp filter_blocks(blocks, term, list \\ [])
+
+  defp filter_blocks(blocks, nil, _), do: blocks
+
+  defp filter_blocks(blocks, filter, list) do
+    blocks
+    |> Enum.reduce(list, fn block, list ->
+      cond do
+        block.type == "list" ->
+          filter_blocks(block.blocks, filter, list)
+        Block.matches_filter?(block, filter) ->
+          list ++ [block]
+        true ->
+          list
+      end
+    end)
+  end
+
   defp extract_canvas_id(url) do
     with match when is_map(match) <- Regex.named_captures(@canvas_regex, url) do
       match["id"]
     end
+  end
+
+  defp get_query(url) do
+    url
+    |> URI.parse
+    |> Map.get(:query)
+    |> Kernel.||("")
+    |> URI.decode_query
   end
 end
--- web/models/block.ex
+++ web/models/block.ex
@@ -18,15 +18,44 @@ defmodule CanvasAPI.Block do
   @doc """
   Builds a changeset based on `struct` and `params`.
   """
+  @spec changeset(%__MODULE__{}, map) :: Ecto.Changeset.t
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:content, :meta, :type])
     |> cast_embed(:blocks)
   end
 
+  @doc """
+  Converts a Block into a plain map with content, meta, type, blocks key.
+  """
+  @spec to_params(%__MODULE__{}) :: map
   def to_params(struct) do
     struct
     |> Map.take([:content, :meta, :type])
     |> Map.put(:blocks, Enum.map(struct.blocks, &to_params/1))
+  end
+
+  @doc """
+  Return whether a block matches a given filter string.
+  """
+  @spec matches_filter?(%__MODULE__{}, String.t) :: boolean
+  def matches_filter?(block = %__MODULE__{type: "url"}, filter) do
+    do_matches_filter?(block.meta["url"], filter)
+  end
+
+  def matches_filter?(block = %__MODULE__{type: "list"}, filter) do
+    block.blocks
+    |> Enum.any?(&matches_filter?(&1, filter))
+  end
+
+  def matches_filter?(block, filter) do
+    do_matches_filter?(block.content, filter)
+  end
+
+  @spec do_matches_filter?(String.t, String.t) :: boolean
+  defp do_matches_filter?(string, term) do
+    string
+    |> String.downcase
+    |> String.contains?(String.downcase(term))
   end
 end
