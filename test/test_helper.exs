50499fa4579b9226e6e1300f50e5f8f214f280b1
Jonathan Clem <jotclem@gmail.com>
2016-10-20 17:26:40 -0400

    Revert "Use TAP in CI"
    This reverts commit de6db4d66bf0cf2444077087943f88f3dd15d34d.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -5,12 +5,7 @@ System.put_env("SLACK_CLIENT_ID", String.duplicate("x", 8) <> "." <> String.dupl
 System.put_env("SLACK_CLIENT_SECRET", String.duplicate("x", 32))
 System.put_env("REDIRECT_URI", "http://localhost:4200")
 
-if System.get_env("HEROKU_TEST_RUN_ID") do
-  ExUnit.configure formatters: [Spout, ExUnit.CLIFormatter]
-else
-  ExUnit.configure formatters: [ExUnit.CLIFormatter, ExUnitNotifier]
-end
-
+ExUnit.configure formatters: [ExUnit.CLIFormatter, ExUnitNotifier]
 ExUnit.start
 
 Ecto.Adapters.SQL.Sandbox.mode(CanvasAPI.Repo, :manual)
