46554eaab410c08398b86174b570f3de0d462350
Jonathan Clem <jonathan@jclem.net>
2016-10-24 20:29:22 -0400

    Document user service

--- lib/canvas_api/services/user_service.ex
+++ lib/canvas_api/services/user_service.ex
@@ -1,7 +1,30 @@
 defmodule CanvasAPI.UserService do
+  @moduledoc """
+  A service for viewing and manipulating users.
+  """
+
   use CanvasAPI.Web, :service
   alias CanvasAPI.User
 
+  @doc """
+  Insert a new user from the given params.
+
+  The creator must provide an account and a team.
+
+  Options:
+
+  - `account`: `%Account{}` (**required**) The account the user will be tied to
+  - `team`: `%Team{}` (**required**) The team the user will be tied to
+
+  ## Examples
+
+  ```elixir
+  UserService.create(
+    %{"email" => "user@example.com"},
+    account: current_account,
+    team: current_team)
+  ```
+  """
   def insert(params, opts) do
     %User{}
     |> User.changeset(params)
