dc047f11432d587a5984d3eede385a4114fbc7d4
Jonathan Clem <jotclem@gmail.com>
2016-11-01 11:14:25 -0400

    Do not return ID in signature attributes

--- web/views/upload_signature_view.ex
+++ web/views/upload_signature_view.ex
@@ -3,7 +3,11 @@ defmodule CanvasAPI.UploadSignatureView do
 
   def render("show.json", %{signature: signature}) do
     signature
-    |> resource_object(Map.take(signature, Map.keys(signature)))
+    |> resource_object(%{
+         policy: signature.policy,
+         signature: signature.signature,
+         upload_url: signature.upload_url
+       })
     |> json_object
   end
 end
